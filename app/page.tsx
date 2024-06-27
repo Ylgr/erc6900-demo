5b23347f49a3c328a3a76dfc1119ad25f6eecaaa
viet tu <quyen.luminous@gmail.com>
2024-06-27 14:43:20 +0700

    (!) done basic flow of multisigs

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,24 @@ export default function Home() {
     const amount = 1000000000000000000n
     const threshold = 2n;
 
+    const operationParameters = {
+        target: bicAddress,
+        value: 0,
+        data: encodeFunctionData({
+            abi: parseAbi([
+                'function transfer(address to, uint256 amount)'
+            ]),
+            functionName: 'transfer',
+            args: [toAddress, amount]
+        }),
+    }
+
+    // const operationParameters = {
+    //     target: '0xeaBcd21B75349c59a4177E10ed17FBf2955fE697',
+    //     value: 0,
+    //     data: '0x'
+    // }
+
     const owners = [
         '0xeaBcd21B75349c59a4177E10ed17FBf2955fE697',
         '0xF4402fE2B09da7c02504DC308DBc307834CE56fE',
@@ -74,22 +92,8 @@ export default function Home() {
     }
 
     const proposeOperation = async () => {
-        // const operationParameters = {
-        //     target: bicAddress,
-        //     value: 0,
-        //     data: encodeFunctionData({
-        //         abi: parseAbi([
-        //             'function transfer(address to, uint256 amount)'
-        //         ]),
-        //         functionName: 'transfer',
-        //         args: [toAddress, amount]
-        //     }),
-        // }
-        const operationParameters = {
-            target: '0xeaBcd21B75349c59a4177E10ed17FBf2955fE697',
-            value: 0,
-            data: '0x'
-        }
+
+
         console.log('operationParameters: ', operationParameters)
         const operation = await multisigAccountClient.proposeUserOperation({uo: operationParameters});
         console.log('operation: ', operation)
@@ -116,7 +120,23 @@ export default function Home() {
         // }
         // console.log('operationParameters: ', operationParameters)
         console.log('userOperationRequest: ', userOperationRequest)
-        const uo = await multisigAccountClient.sendRawUserOperation(userOperationRequest, '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789')
+        // const signedOp = await multisigAccountClient.signUserOperation({uoStruct: userOperationRequest})
+        console.log('signedOp: ', {
+                    uo: operationParameters,
+                    context: {
+                        aggregatedSignature: userOperationRequest.signature,
+                        signatures: signatureOps,
+                        userOpSignatureType: "ACTUAL"
+                    }
+                })
+        const uo = await multisigAccountClient.sendUserOperation({
+            uo: operationParameters,
+            context: {
+                aggregatedSignature: userOperationRequest.signature,
+                signatures: signatureOps,
+                userOpSignatureType: "ACTUAL"
+            }
+        });
         const txHash = await multisigAccountClient.waitForUserOperationTransaction(uo);
         console.log(txHash);
     }
@@ -158,3 +178,6 @@ export default function Home() {
       </main>
   );
 }
+
+"0x000000000000000000000000000000000000000000000000000000009cfa20700000000000000000000000000000000000000000000000000000000013ab66800000000000000000000000000000000000000000000000000000000000989680649b30b4728d1ad2dd6b4b265fcd20878ff763e583c004b9aa2dc9b410013ffe0bf1e90a233ae4a0e99339d18b35b1fc77a7b523f013da8fd4006bc752f7181d1b354db785803cd25b567321aae14aaa45a2b0e0f867ccdfdcd0dc4ffbf8bbb8db1b4b7142a5084cfc94b8aef7f61fc509c7773b2e0b7b22c11f0ff7fcd2f04e381c"
+"0x5285ef77486985e17de423adfc85977d118ebaad129c16a9e831b262767aeb8c38229453aa78d0eaf5a575b1d200004757376caa197ae7e2c915081918cc5c9d1c"
